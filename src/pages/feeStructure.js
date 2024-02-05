2330b372f4f1dd245894cec0cac1e5d6f9255d23
Aditya_Raj-24 <adityajack8210@gmail.com>
2024-02-05 21:55:38 +0530

    feestructure

--- src/pages/feeStructure.js
+++ src/pages/feeStructure.js
@@ -1,24 +1,9 @@
 import React, { useState } from 'react';
 
-const FeeStructure = () => {
-  const [activeMonth, setActiveMonth] = useState(null);
-
-  const handleMonthClick = (month) => {
-    setActiveMonth(activeMonth === month ? null : month);
-  };
-
   return (
-    <div className=" flex justify-between w-80 mx-auto my-20 flex-col">
-      {['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'].map((month) => (
-        <div
-          key={month}
-          className={`cursor-pointer p-4 border border-gray-300 rounded ${activeMonth === month ? 'bg-gray-200 font-bold' : ''}`}
-          onClick={() => handleMonthClick(month)}
-        >
-          {month}
-        </div>
-      ))}
-    </div>
+<div>
+
+</div>
   );
 };
 
