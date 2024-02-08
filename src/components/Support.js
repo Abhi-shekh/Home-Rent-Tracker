867da160a7eca2cedd6a554c8042d36ce689a6f3
Aditya_Raj-24 <adityajack8210@gmail.com>
2024-02-08 21:16:42 +0530

    Committing local changes

---
+++ src/components/Support.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+
+
+    const Support = () => {
+    const navigate = useNavigate()
+
+    function clickHandler() {
+        navigate("/Home")
+    }
+    function BackFunction() {
+        navigate(-1);
+    }
+
+
+    return(
+        <div>
+            <div>
+
+            <h1> IF PAYMENT FAILED PLEASE IMMEDIATLY CONTACT THROUGH --</h1>
+
+
+            <div className="container">
+            <p> *If any payment  <span className="highlight"> fails </span> or money is deducted from your bank 
+            then there is no need to worry, please visit 3rd floor and file your complaint. or contatct -----</p>
+
+            <p> *Contact =<span> +91 8210310051</span></p>
+
+            <p> *Email-  <span> adityajack8210@gmail.com </span> </p>
+            </div>
+
+        
+            <Link to="/ComplaintForm" className="link">
+        Make a complaint 
+                </Link>
+
+
+            </div>
+            <button onClick={clickHandler} 
+            className="link">
+                Move to Home page  
+            </button>
+            <br/>
+            <br/>
+            <button onClick={BackFunction}
+            className="link">
+            Back
+            </button>
+        </div>
+    )
+}
+
+
+export default Support 
--- src/pages/feeStructure.js
+++ src/pages/feeStructure.js
@@ -1,9 +1,98 @@
 import React, { useState } from 'react';
+import { VscCircleSmallFilled } from "react-icons/vsc";
+
+const FeeStructure = () => {
+  const [showDetails, setShowDetails] = useState(false);
+  const [shoefeb, setshowFeb] = useState(false);
+
+
+  const handleHover = () => {
+    setShowDetails(true);
+  };
+  const febHandler = () => {
+    setshowFeb(true);
+  }
+
+
+  const handleLeave = () => {
+    setShowDetails(false);
+  };
+  const febAdd = () => {
+    setshowFeb(false);
+  }
+
+  const clickedhandler = () => {
+    setShowDetails(true);
+  };
+  // const febClick = () => {
+  //   setshowFeb(false);
+  // }
 
   return (
-<div>
+    
+    <div className=' w-full h-full bg-neutral-600 gap-10'>
+      <h2 className=" text-slate-300 rounded-lg w-1/2 text-center 
+       text-1xl font-bold mx-auto">Fee Structure</h2>
 
-</div>
+      <div  className=" bg-white rounded-md w-[16px] mx-auto"
+        onMouseEnter={handleHover}
+        onMouseLeave={handleLeave}
+        onClick={clickedhandler}
+        >
+          <VscCircleSmallFilled />
+      </div>
+        
+        
+      {showDetails && ( 
+        <div className='bg-pink-200 p-4 flex flex-col'>
+          <h3 className='bg-white rounded-md w-1/2 text-center text-1xl font-bold mx-auto'>January</h3>
+          <br/>
+          <p className=" bg-white rounded-lg w-1/2 text-center 
+       text-1xl font-bold mx-auto">Rent: RS 4500</p>
+          <br/>
+
+          <p className=" bg-white rounded-lg w-1/2 text-center 
+       text-1xl font-bold mx-auto">Electric bill: Upload image of your electric meter</p>
+
+          <br/>
+          <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg w-1/2 text-center 
+        text-1xl font-bold mx-auto">
+            Pay Rent
+          </button>
+        </div>
+        
+      )}
+      
+
+      <div  className=" bg-white rounded-md w-[16px] mx-auto"
+        onMouseEnter={febHandler}
+        onMouseLeave={febAdd}
+        // onClick={febClick}
+        >
+          <VscCircleSmallFilled />
+      </div>
+        
+        
+      {setshowFeb && ( 
+        <div className='bg-pink-200 p-4 flex flex-col'>
+          <h3 className='bg-white rounded-md w-1/2 text-center text-1xl font-bold mx-auto'>FEB</h3>
+          <br/>
+          <p className=" bg-white rounded-lg w-1/2 text-center 
+       text-1xl font-bold mx-auto">Rent: RS 4500</p>
+          <br/>
+
+          <p className=" bg-white rounded-lg w-1/2 text-center 
+       text-1xl font-bold mx-auto">Electric bill: Upload image of your electric meter</p>
+
+          <br/>
+          <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg w-1/2 text-center 
+        text-1xl font-bold mx-auto">
+            Pay Rent
+          </button>
+        </div>
+        
+      )}
+    </div>
   );
 };
 
